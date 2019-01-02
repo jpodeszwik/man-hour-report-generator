90c4857e7b886ab5d2fd5e0bbea2c352d3cfce22
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2019-01-02 18:30:30 +0100

    added readme, and fixed local-run.js

---
+++ README.md
@@ -0,0 +1,19 @@
+# Man hour report generator
+Amazon lambda function that generates working hours reports.
+
+## Build
+Install dependencies first:
+
+```yarn install```
+
+Then run webpack
+
+```webpack```
+
+## Run locally
+first build, and then run
+
+```node local-run.js```
+
+## Deploy
+Copy contents of dist directory into amazon lambda function code
--- local-run.js
+++ local-run.js
@@ -1,6 +1,26 @@
-const index = require('./index');
-const body = {"name": "Firstname Lastname", "month": "December", "year": 2018, "tasks": [	{ "hours": 160, "task": "Task name" }]};
+process.chdir('dist');
+const index = require('./dist/index');
+const fs = require('fs');
 
-const result = index.handler({body: JSON.stringify(body)});
+const processResult = result => {
+	const { statusCode, body } = result;
 
-console.log(JSON.stringify(result));
+	if (statusCode === 200) {
+		const buffer = Buffer.from(body, 'base64');
+		fs.writeFile('../test.pdf', buffer, err => {
+			if (err === null) {
+				console.log('finished writing test.pdf file');
+			} else {
+				console.error(`error occured during writing file ${error}`)
+			}
+		});
+	} else {
+		console.error(`statusCode: ${statusCode}, message: ${body}`);
+	} 
+}
+
+const body = {"name": "Firstname Lastname", "month": "December", "year": 2018, "tasks": [{ "hours": 160, "task": "Task name" }]};
+
+index.handler({body: JSON.stringify(body)})
+	.then(processResult);
+
